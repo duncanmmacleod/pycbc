959e79cc33397dda5e5516a5fc7bd094452f47ac
Duncan Macleod <macleoddm@cardiff.ac.uk>
2024-07-23 15:24:35 +0100

    setup.py: add Python 3.12 classifier

--- setup.py
+++ setup.py
@@ -321,6 +321,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Topic :: Scientific/Engineering',
